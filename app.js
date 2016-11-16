c0698c3f18ee035b0fd3922323f89d2bf74129e1
Brigitte Huneke <brigittehuneke@gmail.com>
2016-11-16 15:15:21 -0800

    no totals yet

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var cookiesByHour = [];
 var stores = [];
+var totalSalesArray = [];
 
 /*var storeArray = function() {
   for (var i = 0; i < storeHours.length; i++){
@@ -45,21 +46,16 @@ CookieStore.prototype.totalSalesPerLocation = function() {
   return this.total;
 };
 
-
-/*WORKING ON THE STRETCH GOAL HERE...
-var storeArray = [];
-var makeStoreArray = function() {
-  for (var i = 0; i)
-storeArray =
-function totalSalesPerHour () {
+function totalSalesPerHour() {
   var totalAllStores = 0;
   for (var i = 0; i < storeHours.length; i++) {
-    for (var i = 1; i < 4; i++) {
-       totalAllStores += cookieArray[0];
+    for(var y = 0; y < stores.length; i++) {
+      totalAllStores += ;
     }
   }
   return totalAllStores;
-};*/
+
+}
 
 //make array of all store locations
 //go over each store and for each store add up the cookies in that slot in the array
@@ -72,17 +68,19 @@ storeForm.addEventListener('submit', handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   var storeName = event.target.store_name.value;
-  var minHourlyCustomers = event.target.min_cust.value;
-  var maxHourlyCustomers = event.target.max_cust.value;
-  var avgCookiesPerCustomer = event.target.avg_cookies;
+  var minHourlyCustomers = parseInt(event.target.min_cust.value);
+  var maxHourlyCustomers = parseInt(event.target.max_cust.value);
+  var avgCookiesPerCustomer = parseInt(event.target.avg_cookies.value);
 
   var newStore = new CookieStore(storeName, minHourlyCustomers, maxHourlyCustomers, avgCookiesPerCustomer);
   stores.push(newStore);
+  console.log('MIN: ' + typeof minHourlyCustomers);
+  newStore.toHtml();
 
   event.target.store_name.value = '';
   event.target.min_cust.value = '';
   event.target.max_cust.value = '';
-  event.target.avg_cookies = '';
+  event.target.avg_cookies.value = '';
 }
 
 function renderHeaderRow () {
@@ -130,17 +128,32 @@ CookieStore.prototype.toHtml = function (){
   tableRow.appendChild(totalTableData);
 
   storeTable.appendChild(tableRow);
+
 };
 
-//FOR STRETCH GOAL
-/* function renderFooterRow(){
+function renderFooterRow(){
   var storeTable = document.getElementById('store_table');
   var tableRow = document.createElement('tr');
   var totalTableFooter = document.createElement('th');
+  var grandTotalTableFooter = document.createElement('th');
   var hourlyTableFooter;
 
-  for
-}; */
+  totalTableFooter.textContent = 'Sales per Hour';
+  tableRow.appendChild(totalTableFooter);
+
+  for (var i = 0; i < storeHours.length; i++) {
+    hourlyTableFooter = document.createElement('td');//create element
+    hourlyTableFooter.textContent = totalSalesByHour;//update content
+    tableRow.appendChild(hourlyTableFooter);//put it somewhere
+  }
+
+  grandTotalTableFooter.textContent = 'ALL TOTAL';
+  tableRow.appendChild(grandTotalTableFooter);
+
+  storeTable.appendChild(tableRow);
+}
+
+
 
 /* put all objects into an array
 iterate over that array
@@ -150,18 +163,23 @@ add them together while walking through
 
 var pike = new CookieStore('1st and Pike', 23, 65, 6.3);
 console.log(pike);
+stores.push(pike);
 
 var seaTacAirport = new CookieStore('SeaTac Airport', 3, 24, 1.2);
 console.log(seaTacAirport);
+stores.push(seaTacAirport);
 
 var seattleCenter = new CookieStore('Seattle Center', 11, 38, 3.7);
 console.log(seattleCenter);
+stores.push(seattleCenter);
 
 var capitolHill = new CookieStore('Capitol Hill', 20, 38, 2.3);
 console.log(capitolHill);
+stores.push(capitolHill);
 
 var alki = new CookieStore('Alki', 2, 16, 4.6);
 console.log(alki);
+stores.push(alki);
 
 pike.toHtml();
 
@@ -198,6 +216,10 @@ console.log('randomHourlyCustomers: ' + alki.randomHourlyCustomers());
 console.log('cookiesPurchasedHourly: ' + alki.cookiesPurchasedHourly());
 console.log('totalSalesPerLocation: ' + alki.totalSalesPerLocation());
 console.log('cookieArray: ' + alki.cookieArray);
+
+totalSalesPerHour();
+totalSalesArray.push(totalAllStores);
+renderFooterRow();
 
 //console.log('cookiesByHour: ' + cookiesByHour);
 
